9dad3b8a00d2d03b4632b61de1aaaa205655515c
Luiz Matos <lfmatosmelo@id.uff.br>
2019-10-18 00:46:09 -0300

    WIP
    - inserção de inimigos usando pathfinding

--- Mario/Assets/Scripts/EnemyAI.cs
+++ Mario/Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 using Pathfinding;
 
 [RequireComponent (typeof(Rigidbody2D))]
@@ -12,20 +13,74 @@ public class EnemyAI : MonoBehaviour
     public Path path;
     public float speed = 300f;
     public ForceMode2D fMode;
+    public int currentWaypoint = 0;
+    public float nextWaypointDistance = 3;
 
     [HideInInspector]
     private bool pathIsEnded = false;
 
-    public float nextWaypointDistance = 3;
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        rgb2d = GetComponent<Rigidbody2D>();
+
+        if (target == null) {
+            Debug.LogError('Jogador não encontrado');
+            return;
+        }
+
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
     }
 
-    // Update is called once per frame
+    IENumerator UpdatePath() {
+        if (target = null) {
+            return false;
+        }
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+        yield new return WaitForSeconds(1f/updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    private void OnPathComplete(Path p) {
+        if (!p.error) {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (target = null) {
+            return;
+        }
+
+        if (path == null) {
+            return;
+        }
+
+        if (currentWaypoint >= path.vectorPath.Count) {
+            if (pathIsEnded) return;
+
+            Debug.Log('End of path reached');
+            pathIsEnded = true;
+            return;
+        }
+
+        pathIsEnded = false;
+
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized();
+        dir *= speed * Time.fixedDeltaTime();
+
+        rgb2d.AddForce(dir, fMode);
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint];
+        if (dist < nextWaypointDistance) {
+            currentWaypoint++;
+        }
+    }
+    
     void Update()
     {
         
